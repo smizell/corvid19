9c6672b81b73af1fb5d676129f520250b716a757
Stephen Mizell <smizell@gmail.com>
2020-03-15 14:50:31 -0500

    Markdown and Jinja rendering

--- site.py
+++ site.py
@@ -1,10 +1,19 @@
+import os
 import frontmatter
+import markdown
 import jinja2
 
 
 class Site:
-    def __init__(self, renderer):
+    def __init__(self, renderer, collection):
         self.renderer = renderer
+        self.collection = collection
+
+    def render_collection(self):
+        for doc in self.collection.docs:
+            if doc.file_name.endswith('.md'):
+                doc.info.content = markdown.markdown(doc.info.content)
+            doc.info.content = self.renderer.render(doc)
 
 
 class Renderer:
@@ -18,15 +27,34 @@ class Renderer:
 
 
 class Document:
-    def __init__(self, filename, info):
+    def __init__(self, file_name, info):
+        self.file_name = file_name
         self.info = info
 
     @classmethod
-    def from_filename(cls, filename):
-        return cls(filename, frontmatter.load(filename))
+    def from_file_name(cls, file_name):
+        return cls(file_name, frontmatter.load(file_name))
+
+    def __repr__(self):
+        return f'Document({self.file_name})'
 
 
-site = Site(renderer=Renderer())
-doc = Document.from_filename('content/index.md')
+class Collection:
+    def __init__(self, dir_name, docs):
+        self.dir_name = dir_name
+        self.docs = docs
 
-print(site.renderer.render(doc))
+    @classmethod
+    def from_dir_name(cls, dir_name):
+        docs = []
+        for root, dirs, files in os.walk(dir_name):
+            for f in files:
+                docs.append(Document.from_file_name(os.path.join(root, f)))
+        return cls(dir_name, docs)
+
+
+
+collection = Collection.from_dir_name('./content')
+site = Site(renderer=Renderer(), collection=collection)
+site.render_collection()
+print(site.collection.docs[0].info.content)
