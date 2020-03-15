1d574468d4eadc3cbc781a5a6f704d070edfb78a
Stephen Mizell <smizell@gmail.com>
2020-03-15 14:31:09 -0500

    Add handlers

--- site.py
+++ site.py
@@ -1,10 +1,32 @@
 import frontmatter
 import jinja2
 
-index = frontmatter.load('content/index.md')
 
-templateLoader = jinja2.FileSystemLoader(searchpath="./layouts")
-templateEnv = jinja2.Environment(loader=templateLoader)
-template = templateEnv.get_template('main.jinja2')
+class Site:
+    def __init__(self, renderer):
+        self.renderer = renderer
 
-print(template.render(content=index.content))
+
+class Renderer:
+    def __init__(self):
+        loader = jinja2.FileSystemLoader(searchpath="./layouts")
+        self.template_env = jinja2.Environment(loader=loader)
+        self.template = self.template_env.get_template('main.jinja2')
+
+    def render(self, doc):
+        return self.template.render(doc=doc)
+
+
+class Document:
+    def __init__(self, filename, info):
+        self.info = info
+
+    @classmethod
+    def from_filename(cls, filename):
+        return cls(filename, frontmatter.load(filename))
+
+
+site = Site(renderer=Renderer())
+doc = Document.from_filename('content/index.md')
+
+print(site.renderer.render(doc))
