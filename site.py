a85527004dd1932176a2e500f590b07a0ab6676a
Stephen Mizell <smizell@gmail.com>
2020-03-15 15:40:10 -0500

    Add build code

--- site.py
+++ site.py
@@ -1,7 +1,13 @@
 import os
+import pathlib
+import shutil
 import frontmatter
 import markdown
 import jinja2
+
+BUILD_DIR = './build'
+CONTENT_DIR = './content'
+LAYOUTS_DIR = './layouts'
 
 
 class Site:
@@ -13,10 +19,22 @@ class Site:
         for doc in self.collection.docs:
             doc.info.content = self.renderer.render(doc)
 
+    def persist(self, dir_name=BUILD_DIR):
+        if os.path.exists(dir_name):
+            shutil.rmtree(dir_name)
+        for doc in self.collection.docs:
+            path = doc.dir_name.replace('./content', './build')
+            if os.path.exists(path) == False:
+                pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+            new_full_path = os.path.join(path, doc.file_name)
+            with open(new_full_path, 'w+') as f:
+                f.write(doc.info.content)
+
+
 
 class Renderer:
     def __init__(self):
-        loader = jinja2.FileSystemLoader(searchpath="./layouts")
+        loader = jinja2.FileSystemLoader(searchpath=LAYOUTS_DIR)
         self.template_env = jinja2.Environment(loader=loader)
         self.templates = {
             # 'main': self.template_env.get_template('main.jinja2'),
@@ -27,6 +45,7 @@ class Renderer:
         # Markdown uses the page template
         if doc.file_name.endswith('.md'):
             doc.info.content = markdown.markdown(doc.info.content)
+            doc.file_name = doc.file_name.replace('.md', '.html')
             return self.templates['page'].render(doc=doc)
 
         # Jinja2 files will render as themselves
@@ -34,20 +53,22 @@ class Renderer:
         # can be used to load other templates
         if doc.file_name.endswith('.jinja2'):
             template = self.template_env.from_string(doc.info.content)
+            doc.file_name = doc.file_name.replace('.jinja2', '.html')
             return template.render(doc=doc)
 
 
 class Document:
-    def __init__(self, file_name, info):
+    def __init__(self, dir_name, file_name, info):
+        self.dir_name = dir_name
         self.file_name = file_name
         self.info = info
 
     @classmethod
-    def from_file_name(cls, file_name):
-        return cls(file_name, frontmatter.load(file_name))
+    def from_file_name(cls, dir_name, file_name):
+        return cls(dir_name, file_name, frontmatter.load(os.path.join(dir_name, file_name)))
 
     def __repr__(self):
-        return f'Document({self.file_name})'
+        return f'Document({self.dir_name}/{self.file_name})'
 
 
 class Collection:
@@ -60,12 +81,11 @@ class Collection:
         docs = []
         for root, dirs, files in os.walk(dir_name):
             for f in files:
-                docs.append(Document.from_file_name(os.path.join(root, f)))
+                docs.append(Document.from_file_name(root, f))
         return cls(dir_name, docs)
 
 
-
-collection = Collection.from_dir_name('./content')
+collection = Collection.from_dir_name(CONTENT_DIR)
 site = Site(renderer=Renderer(), collection=collection)
 site.render_collection()
-print(site.collection.docs[1].info.content)
+site.persist()
